86bdfcc170f2357224ee2e8cdda2241507c7ab60
AlexSG23 <dasanabria0323@gmail.com>
2025-11-05 21:06:58 -0500

    Add slider for number of coin tosses
    Agrega control deslizante para elegir número de lanzamientos

--- app.py
+++ app.py
@@ -2,3 +2,10 @@ import streamlit as st
 
 st.header('Lanzar una moneda')
 st.write('Esta aplicación aún no es funcional. En construcción.')
+num_lanzamientos = st.slider(
+    "¿Cuántas veces quieres lanzar la moneda?",
+    min_value=1,
+    max_value=100,
+    value=10,
+    step=1
+)
