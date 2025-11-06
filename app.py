ec79e9a81190a7389eea85d64daf82b9d7a2e0bf
AlexSG23 <dasanabria0323@gmail.com>
2025-11-05 21:20:27 -0500

    Update coin toss app with new slider and button

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 import streamlit as st
 
 st.header('Lanzar una moneda')
+
+number_of_trials = st.slider('¿Número de intentos?', 1, 1000, 10)
+start_button = st.button('Ejecutar')
+
+if start_button:
+    st.write(f'Experimento con {number_of_trials} intentos en curso.')
+
 st.write('Esta aplicación aún no es funcional. En construcción.')
-num_lanzamientos = st.slider(
-    "¿Cuántas veces quieres lanzar la moneda?",
-    min_value=1,
-    max_value=100,
-    value=10,
-    step=1
-)
